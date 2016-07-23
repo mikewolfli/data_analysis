3a2be5640b78ccda9d6dafbf89ef674523dbbef2
mikewolfli <mikewolfli@163.com>
2016-07-23 11:34:32 +0800

    update

--- FinTool/constant.py
+++ FinTool/constant.py
@@ -406,5 +406,155 @@ area_classified_dic = {
                        'area':'地域名称',                    
                        }
 
+'''
+中小板分类¶
+获取中小板股票数据，即查找所有002开头的股票
 
+创业板分类
+获取创业板股票数据，即查找所有300开头的股票
 
+风险警示板分类¶
+获取风险警示板股票数据，即查找所有st股票
+
+上证50成份股
+获取上证50成份股
+
+中证500成份股
+获取中证500成份股
+'''
+five_classified_dic = {
+                      'code':'股票代码',
+                      'name':'股票名称',                     
+                      }
+
+'''
+沪深300成份及权重
+获取沪深300当前成份股及所占权重
+'''
+hs300s_dic={
+            'code':'股票代码',
+            'name':'股票名称',
+            'date':'日期',
+            'weight':'权重',           
+            }
+
+'''
+终止上市股票列表
+获取已经被终止上市的股票列表，数据从上交所获取，目前只有在上海证券交易所交易被终止的股票。
+'''
+terminated_dic = {
+                  'code':'股票代码',
+                  'name':'股票名称',
+                  'oDate':'上市日期',
+                  'tDate':'终止上市日期',                 
+                  }
+
+#基本面数据
+'''
+股票列表
+'''
+stoc_basics_dic = {
+                   'code':'代码',
+                   'name':'名称',
+                   'industry':'所属行业',
+                   'area':'地区',
+                   'pe':'市盈率',
+                   'outstanding':'流通股本',
+                   'totals':'总股本(万)',
+                   'totalAssets':'总资产(万)',
+                   'liquidAssets':'流动资产',
+                   'fixedAssets':'固定资产',
+                   'reserved':'公积金',
+                   'reservedPerShare':'每股公积金',
+                   'eps':'每股收益',
+                   'bvps':'每股净资',
+                   'pb':'市净率',
+                   'timeToMarket':'市日期',                  
+                   }
+
+'''
+业绩报告（主表）
+'''
+report_data_dic = {
+                   'code':'代码',
+                   'name':'名称',
+                   'eps':'每股收益',
+                   'eps_yoy':'每股收益同比(%)',
+                   'bvps':'每股净资产',
+                   'roe':'净资产收益率(%)',
+                   'epcf':'每股现金流量(元)',
+                   'net_profits':'净利润(万元)',
+                   'profits_yoy':'净利润同比(%)',
+                   'distrib':'分配方案',
+                   'report_date':'发布日期',                  
+                   }
+
+'''
+盈利能力
+'''
+profit_data_dic = {
+                   'code':'代码',
+                   'name':'名称',
+                   'roe':'净资产收益率(%)',
+                   'net_profit_ratio':'净利率(%)',
+                   'gross_profit_rate':'毛利率(%)',
+                   'net_profits':'净利润(万元)',
+                   'eps':'每股收益',
+                   'business_income':'营业收入(百万元)',
+                   'bips':'每股主营业务收入(元)',                   
+                   }
+
+'''
+营运能力
+'''
+operation_data_dic = {
+                      'code':'代码',
+                      'name':'名称',
+                      'arturnover':'应收账款周转率(次)',
+                      'arturndays':'应收账款周转天数(天)',
+                      'inventory_turnover':'存货周转率(次)',
+                      'inventory_days':'存货周转天数(天)',
+                      'currentasset_turnover':'流动资产周转率(次)',
+                      'currentasset_days':'流动资产周转天数(天) ',                    
+                      }
+
+'''
+成长能力
+'''
+growth_data_dic = {
+                   'code':'代码',
+                   'name':'名称',
+                   'mbrg':'主营业务收入增长率(%)',
+                   'nprg':'净利润增长率(%)',
+                   'nav':'净资产增长率',
+                   'targ':'总资产增长率',
+                   'epsg':'每股收益增长率',
+                   'seg':'股东权益增长率',                  
+                   }
+
+'''
+偿债能力
+'''
+debpaying_data_dic = {
+                      'code':'代码',
+                      'name':'名称',
+                      'currentratio':'流动比率',
+                      'quickratio':'速动比率',
+                      'cashratio':'现金比率',
+                      'icratio':'利息支付倍数',
+                      'sheqratio':'股东权益比率',
+                      'adratio':'股东权益增长率',                     
+                      }
+
+'''
+现金流量
+'''
+cashflow_data_dic = {
+                     'code':'代码',
+                     'name':'名称',
+                     'cf_sales':'经营现金净流量对销售收入比率',
+                     'rateofreturn':'资产的经营现金流量回报率',
+                     'cf_nm':'经营现金净流量与净利润的比率',
+                     'cf_liabilities':'经营现金净流量对负债比率',
+                     'cashflowratio':'现金流量比率',                    
+                     }
